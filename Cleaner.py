ab21ee29158fa2b95e214e813657c720a0198c7b
Brandon Montalvo <79609464+brand0nM@users.noreply.github.com>
2022-08-12 10:46:26 -0600

    Add files via upload

--- Cleaner.py
+++ Cleaner.py
@@ -10,64 +10,67 @@ import time
 
 
 
-def reverseLookup(mvp_awards):
-    player_names = playerid_reverse_lookup(mvp_awards['playerID'], key_type='bbref')
-    player_names['Name'] = player_names['name_first'].map(
-        lambda x: x.title()) + ' ' + player_names['name_last'].map(lambda x: x.title())
-    player_names = player_names[['Name', 'key_bbref','mlb_played_first', 'mlb_played_last']]
-    
-    return player_names.merge(mvp_awards, left_on= 'key_bbref', right_on='playerID').drop(['key_bbref'], axis=1)
+def reverseLookup(mvp_awards): # Player Names based on bbref id
+	player_names = playerid_reverse_lookup(mvp_awards['playerID'], key_type='bbref') # PyBaseball Function 
+	player_names['Name'] = player_names['name_first'].map( # Create the Name Column
+	lambda x: x.title()) + ' ' + player_names['name_last'].map(lambda x: x.title()) # Append first and last name
+	player_names = player_names[['Name', 'key_bbref','mlb_played_first', 'mlb_played_last']] # Select columns
+
+	return player_names.merge(mvp_awards, left_on= 'key_bbref', right_on='playerID').drop(['key_bbref'], axis=1)
 
 
 
 
-def Merger(start_year, end_year, mvp_awards, all_awards, league_type):
-    seconds = time.time()
-    war = bwar_bat()  
+def Merger(start_year, end_year, mvp_awards, all_awards, league_type): # Preprocess MVP for Machine learning
+	seconds = time.time()
+	war = bwar_bat() # Get war using Pybaseball function
 
-    for i in range(end_year-start_year +1):
-        current_year = start_year + i
-        if i < 1:
-            data = getStats(current_year, mvp_awards, all_awards, war, league_type)
-            
-        else:
-            new_data = getStats(current_year, mvp_awards, all_awards, war, league_type)
-            data = pd.concat([data, new_data])
+	for i in range(end_year-start_year +1): # Loop through each Year
+		current_year = start_year + i # Define Current Year variable
+		if current_year = 2020: # Skip Covid, less games affects WAR
+			current_year = current_year + 1
 
-    data['Tm'] = data['Tm'].map(lambda x: x.split(',')[-1])
-    print(str(time.time()-seconds)+' Seconds')
-    return data
+		if i < 1: # Base Case: at the start_year 
+			data = getStats(current_year, mvp_awards, all_awards, war, league_type) # Years Stats
 
-def getStats(current_year, mvp_awards, all_awards, war, league_type):
-    data = getPlayersYearStats(current_year, war, league_type)
-    mvpChecker(data, 'Name', mvp_awards, current_year, league_type)
-    
-    return addOtherStats(data, all_awards, current_year)
+		else:
+			new_data = getStats(current_year, mvp_awards, all_awards, war, league_type) # Years Stats
+			data = pd.concat([data, new_data]) # Combine old and new stats 
+			data['Tm'] = data['Tm'].map(lambda x: x.split(',')[-1]) # Most recent players team
 
-def getPlayersYearStats(current_year, war, league_type):
-    war = war.loc[war['year_ID'] == current_year][['year_ID','name_common','WAR']]
-    batting_stats = batting_stats_range(str(current_year)+'-04-07', str(current_year)+'-10-05'
-                                       ).merge(war, how='left', left_on='Name', right_on='name_common'
-                                              ).drop(['name_common', 'GDP','SH'], axis=1)
-    batting_stats['Lev'] = batting_stats['Lev'].map(lambda x: x.split(',')[-1])
-    
-    return batting_stats.loc[batting_stats['Lev'] == league_type]
+	print(str(time.time()-seconds)+' Seconds')
+	return data
 
-def mvpChecker(data, column, mvp_awards, current_year, league_type):
-    for mvp in mvp_awards[column].loc[(mvp_awards['yearID'] == current_year) 
-                                      & (mvp_awards['lgID'] == league_type.split('-')[-1])]:
-        data['MVP'] = data[column].map(lambda x: categorizer(mvp, x))
+def getStats(current_year, mvp_awards, all_awards, war, league_type): # Years Stats
+	data = getPlayersYearStats(current_year, war, league_type) # Clean Player Stats
+	mvpChecker(data, 'Name', mvp_awards, current_year, league_type) # Find Players Awarded MVP
+
+	return addOtherStats(data, all_awards, current_year) # Find Awarded Players
+
+def getPlayersYearStats(current_year, war, league_type): # Clean Player Stats
+	war = war.loc[war['year_ID'] == current_year][['year_ID','name_common','WAR']] # Filter, Trim War
+	batting_stats = batting_stats_range(str(current_year)+'-04-07', str(current_year)+'-10-05' # Pybaseball function
+				       ).merge(war, how='left', left_on='Name', right_on='name_common' # Merge War
+						      ).drop(['name_common', 'GDP','SH'], axis=1) # Drop unimportant columns
+	batting_stats['Lev'] = batting_stats['Lev'].map(lambda x: x.split(',')[-1]) # Most recent players league
+
+	return batting_stats.loc[batting_stats['Lev'] == league_type] # Retrieve Players from specified League
+
+def mvpChecker(data, column, mvp_awards, current_year, league_type): # Find Players Awarded MVP
+	for mvp in mvp_awards[column].loc[(mvp_awards['yearID'] == current_year) # Current Year's Awards
+				      & (mvp_awards['lgID'] == league_type.split('-')[-1])]: # Cleaned most recent League
+	data['MVP'] = data[column].map(lambda x: categorizer(mvp, x)) # Add MVP 
         
-def addOtherStats(data, all_awards, current_year):
-    data = data.merge(all_awards.loc[
-        all_awards['yearID'] == current_year][['Name','awardID']
-                                             ], how='left', on='Name')
-    data['awardID'] = data['awardID'].fillna('No Award')
-    data['WAR'] = data['WAR'].fillna(0)   
-    
-    return data
+def addOtherStats(data, all_awards, current_year): # Find Awarded Players
+	data = data.merge(all_awards.loc[ # Merge player data and all awards data
+		all_awards['yearID'] == current_year][['Name','awardID'] # Based on current year and name
+						     ], how='left', on='Name')
+	data['awardID'] = data['awardID'].fillna('No Award') # Fill no award category
+	data['WAR'] = data['WAR'].fillna(0) # Fill no war with average (Okay since less than 1/100 are null)
 
-def categorizer(mvp, player):
+	return data # Return the cleaned data
+
+def categorizer(mvp, player): # Assign 1 or 0 if MVP
         if mvp == player:
             return 1
         else:
@@ -77,8 +80,8 @@ def categorizer(mvp, player):
         
         
         
-def mvpVerify(data):
-    mvp_check = data.loc[data['MVP'] == 1]
-    print('There are/is '+str(mvp_check['Name'].nunique())+' unique mvps')
-    print(mvp_check['Name'].unique())
-    return mvp_check
+def mvpVerify(data): # Filter Data Frame To Check Work 
+	mvp_check = data.loc[data['MVP'] == 1] 
+	print('There are/is '+str(mvp_check['Name'].nunique())+' unique mvps') # Print Unique Count
+	print(mvp_check['Name'].unique()) # Print Unique Names
+	return mvp_check
